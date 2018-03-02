72ca1ba8035ec9b8f3bbdc0319db2ae33949a794
Bagul16 <vandershannon@gmail.com>
2018-03-02 16:07:31 +0000

    Inital

---
+++ DataSets/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataSets
+{
+    public abstract class GenericRepository<C, T> : IGenericRepository<T> where T : class where C : DbContext, new()
+    {
+
+        private C entities = new C();
+        public C Context
+        {
+
+            get { return this.entities; }
+            set { this.entities = value; }
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            var query = this.entities.Set<T>();
+            return query;
+        }
+
+        public IQueryable<T> FindBy(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+
+            var query = this.entities.Set<T>().Where(predicate);
+            return query;
+        }
+
+        public virtual void Add(T entity)
+        {
+            this.entities.Set<T>().Add(entity);
+        }
+
+        public virtual void AddCollection(IEnumerable<T> entity)
+        {
+            this.entities.Set<T>().AddRange(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            this.entities.Set<T>().Remove(entity);
+        }
+
+        public virtual void Edit(T entity)
+        {
+            this.entities.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual async Task AsyncSave()
+        {
+            await this.entities.SaveChangesAsync();
+        }
+
+        public virtual void AddAndSave(T entity)
+        {
+            this.entities.Set<T>().Add(entity);
+            this.entities.SaveChanges();
+        }
+
+        public virtual void Save()
+        {
+            this.entities.SaveChanges();
+        }
+    }
+}
---
+++ DataSets/JsonExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DataSets
+{
+    public static class JsonExtensions
+    {
+        public static string ToJson(this object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+
+        public static T As<T>(this JToken jToken)
+        {
+            return jToken == null ? default(T) : jToken.ToObject<T>();
+        }
+
+        public static T FromJson<T>(this string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str,
+                new JsonSerializerSettings() { Error = (sender, args) => { args.ErrorContext.Handled = true; } });
+        }
+    }
+}
